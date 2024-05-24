30b9a213f5d48239858426c183fdd50821aefed8
João Batista Santos Junior <joao.santos@programmers.com.br>
2024-05-24 14:11:44 -0300

    Add Colors adjustments

--- Financer.UI/src/app/app.component.ts
+++ Financer.UI/src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { Component, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './shared/header/header.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent,RouterOutlet],
+  imports: [CommonModule, HeaderComponent, SidenavComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [{ provide: LOCALE_ID, useValue: 'en-US' }]
 })
 export class AppComponent {
   title = 'Financer.UI';
+  version = '1.0.0';
 }
